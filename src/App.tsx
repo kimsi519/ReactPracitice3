ccc274d2c2d20ef987c78b3ab37467c3cf3bbdf8
Ji-eun Yang <jidak1165@gmail.com>
2024-10-02 16:38:55 +0900

    [feature] 캐러설 구현 01

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
+// 캐러셀 용
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import "./App.css";
 import ProductDetail from "./components/ProductDetail";
 // 기타 필요한 컴포넌트 임포트
