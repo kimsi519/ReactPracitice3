f560eaf671865dff0aca37423148f34ff9636831
suminjeff <139415437+suminjeff@users.noreply.github.com>
2024-10-02 16:24:52 +0900

    feat: Fix wrong import

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "./Loading";
-import cart from "../assets/cart.svg";
 
 interface Rating {
   count: number;
